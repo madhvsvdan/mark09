053668e0f05e59eb84fa366c87871970433c1cae
Madhusudan <96856144+madhusudankulkarni@users.noreply.github.com>
2022-11-01 12:59:11 +0530

    added footer

---
+++ src/Footer.jsx
@@ -0,0 +1,38 @@
+export default function Footer() {
+  return (
+    <div>
+      {' '}
+      <div class='footer-links'>
+        <ul>
+          <li>
+            <a
+              target='_blank'
+              rel='noopener noreferrer'
+              href='https://github.com/madhvsvdan'
+            >
+              GitHub
+            </a>
+          </li>
+          <li>
+            <a
+              target='_blank'
+              rel='noopener noreferrer'
+              href='https://www.linkedin.com/in/madhvsvdan/'
+            >
+              LinkedIn
+            </a>
+          </li>
+          <li>
+            <a
+              target='_blank'
+              rel='noopener noreferrer'
+              href='https://twitter.com/madhvsvdan'
+            >
+              Twitter
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
